7e545bcd3878135e4c2871a952d5a1f1770bbc46
Krystian <krystian14d@gmail.com>
2022-04-01 10:51:30 +0200

    tests parametrization - exercise

--- exercises/src/test/java/pl/javastart/tj/daysinmonth/MonthCalculatorParametrizedTest.java
+++ exercises/src/test/java/pl/javastart/tj/daysinmonth/MonthCalculatorParametrizedTest.java
@@ -13,46 +13,90 @@ class MonthCalculatorParametrizedTest {
     private MonthCalculator monthCalculator = new MonthCalculator();
 
     @ParameterizedTest
-    @ValueSource(strings = { "2001-01", "2001-03", "2001-05", "2001-07", "2001-08", "2001-10", "2001-12"})
+    @ValueSource(strings = {"2001-01", "2001-03", "2001-05", "2001-07", "2001-08", "2001-10", "2001-12"})
     public void shouldReturn31Days(String month) {
-        // given
+        //given
         YearMonth yearMonth = YearMonth.parse(month);
 
-        // when
+        //when
         int dayCount = monthCalculator.getDayCount(yearMonth.getYear(), yearMonth.getMonthValue());
 
-        // then
+        //then
         assertThat(dayCount).isEqualTo(31);
     }
 
     @ParameterizedTest
-    @CsvSource( {"2001, 04", "2001, 06", "2001, 09", "2001, 11" })
+    @CsvSource({"2001, 04", "2001, 06", "2001, 11"})
     public void shouldReturn30Days(int year, int month) {
-        // when
+        //when
         int dayCount = monthCalculator.getDayCount(year, month);
 
-        // then
+        //then
         assertThat(dayCount).isEqualTo(30);
     }
 
     @ParameterizedTest
-    @CsvSource( { "2021, 02"})
-    public void shouldReturn28Days(int year, int month) {
-        // when
+    @CsvSource({"2000, 02", "2016, 02"})
+    public void shouldReturn29Days(int year, int month) {
+        //when
         int dayCount = monthCalculator.getDayCount(year, month);
 
-        // then
-        assertThat(dayCount).isEqualTo(28);
+        //then
+        assertThat(dayCount).isEqualTo(29);
     }
 
     @ParameterizedTest
-    @CsvSource( {"2004, 02", "2000, 02" })
-    public void shouldReturn29Days(int year, int month) {
-        // when
+    @CsvSource({"2100, 02", "2010, 02"})
+    public void shouldReturn28Days(int year, int month){
+        //when
         int dayCount = monthCalculator.getDayCount(year, month);
 
-        // then
-        assertThat(dayCount).isEqualTo(29);
+        //then
+        assertThat(dayCount).isEqualTo(28);
     }
+
+
+//    @ParameterizedTest
+//    @ValueSource(strings = { "2001-01", "2001-03", "2001-05", "2001-07", "2001-08", "2001-10", "2001-12"})
+//    public void shouldReturn31Days(String month) {
+//        // given
+//        YearMonth yearMonth = YearMonth.parse(month);
+//
+//        // when
+//        int dayCount = monthCalculator.getDayCount(yearMonth.getYear(), yearMonth.getMonthValue());
+//
+//        // then
+//        assertThat(dayCount).isEqualTo(31);
+//    }
+//
+//    @ParameterizedTest
+//    @CsvSource( {"2001, 04", "2001, 06", "2001, 09", "2001, 11" })
+//    public void shouldReturn30Days(int year, int month) {
+//        // when
+//        int dayCount = monthCalculator.getDayCount(year, month);
+//
+//        // then
+//        assertThat(dayCount).isEqualTo(30);
+//    }
+//
+//    @ParameterizedTest
+//    @CsvSource( { "2021, 02"})
+//    public void shouldReturn28Days(int year, int month) {
+//        // when
+//        int dayCount = monthCalculator.getDayCount(year, month);
+//
+//        // then
+//        assertThat(dayCount).isEqualTo(28);
+//    }
+//
+//    @ParameterizedTest
+//    @CsvSource( {"2004, 02", "2000, 02" })
+//    public void shouldReturn29Days(int year, int month) {
+//        // when
+//        int dayCount = monthCalculator.getDayCount(year, month);
+//
+//        // then
+//        assertThat(dayCount).isEqualTo(29);
+//    }
 
 }
