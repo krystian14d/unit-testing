91438bc6c6fa1b9998870ce83730b75aa0875b80
Krystian <krystian14d@gmail.com>
2022-04-06 11:52:31 +0200

    argument captors - exercise

---
+++ exercises/src/main/java/pl/javastart/tj/argcaptors1/DateTimeProvider.java
@@ -0,0 +1,10 @@
+package pl.javastart.tj.argcaptors1;
+
+import java.time.ZonedDateTime;
+
+public class DateTimeProvider {
+
+    ZonedDateTime now(){
+        return ZonedDateTime.now();
+    }
+}
--- exercises/src/main/java/pl/javastart/tj/argcaptors1/Signup.java
+++ exercises/src/main/java/pl/javastart/tj/argcaptors1/Signup.java
@@ -9,4 +9,35 @@ class Signup {
     private ZonedDateTime from;
     private ZonedDateTime to;
 
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
+
+    public ZonedDateTime getFrom() {
+        return from;
+    }
+
+    public void setFrom(ZonedDateTime from) {
+        this.from = from;
+    }
+
+    public ZonedDateTime getTo() {
+        return to;
+    }
+
+    public void setTo(ZonedDateTime to) {
+        this.to = to;
+    }
 }
